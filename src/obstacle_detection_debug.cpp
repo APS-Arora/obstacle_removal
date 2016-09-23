dea240b75a617725ced57bf6d38548c8d82855ec
SithAP <apsarora@hotmail.com>
2016-09-23 13:30:38 +0530

    Completing Coding and Debugging for obstacle detection. Testing Pending.
    Input of parameters fixed. Multiple Scan Data can be now used to improve accuracy.
    New Parameters added: max_detect_range for adjusting accuracy by adjusting detection range.
    		      combine_scan for adjusting no. of scans to combine for detection
    Only 3D Perpective Trasform is applied to completely remove the obstacles which is part of another code.

--- src/obstacle_detection_debug.cpp
+++ src/obstacle_detection_debug.cpp
@@ -7,6 +7,8 @@
 #include "opencv2/opencv.hpp"
 #include "vector"
 #include <math.h>
+#include "string"
+#include "deque"
 
 #define Pixel(M,x,y,c) *(M.data+M.step[0]*x+M.step[1]*y+c)
 /**
@@ -29,48 +31,57 @@ int main(int argc, char **argv)
  * Declaration of local Variables
  */
   uchar intensity;
-  int idx, threshold,i,j,k,l,sum_i,sum_xi,sum_yi,*count;  
-  double min_obstacle_rad,max_obstacle_rad,inflation,theta,r,x_c,y_c,r_c;
+  int idx, threshold,i,j,k,l,sum_i,sum_xi,sum_yi,*count,nscans;  
+  double min_obstacle_rad,max_obstacle_rad,inflation,theta,r,x_c,y_c,r_c,range;
   vector<cv::Vec3f> circles;
+  deque<cv::Mat> accs;
   cv::Mat mask=cv::Mat::zeros(1121,2241,CV_8UC1),acc;
+  cv::Point center;
+  string node_name;
 /**
  * ROS initialisations
  */
-  ros::init(argc, argv, "obstacle_removal");
+  ros::init(argc, argv, ros::this_node::getName());
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("scan", 1000, con_fusion);
+  node_name=ros::this_node::getName();
 /**
  *  Accepting all the Parameters to be used by this node
- *  DEBUG PROBLEM: Parameters are not being input properly
  */
-  if(ros::param::get("min_obstacle_rad",min_obstacle_rad))
+  if(!ros::param::get(node_name+"/min_obstacle_rad",min_obstacle_rad))
   {
 	ROS_ERROR("Please specify the minimum threshold for obstacle radius!\nExiting....");
 	return -1;	
   }
-  if(ros::param::get("max_obstacle_rad",max_obstacle_rad))
+  if(!ros::param::get(node_name+"/max_obstacle_rad",max_obstacle_rad))
   {
 	ROS_ERROR("Please specify the maximum threshold for obstacle radius!\nExiting....");
 	return -1;
   }
-  if(ros::param::get("scan_threshold",threshold))
+  if(!ros::param::get(node_name+"/scan_threshold",threshold))
   {	
 	ROS_WARN("No minimum scan points threshold for obstacle detection specified.\nUsing default value!");
-	threshold=100;
+	threshold=500;
   }
-  if(ros::param::get("inflation_factor",inflation))
+  if(!ros::param::get(node_name+"/inflation_factor",inflation))
   {
 	ROS_WARN("Inflation factor for obstacles not defined.\nUsing default value!");
 	inflation=1.0;	
   }
+  if(!ros::param::get(node_name+"/combine_scans",nscans))
+  {
+	ROS_WARN("No. of scans to combine for obstacle detection not defined.\nUsing default value!");
+	nscans=5;	
+  }
+  if(!ros::param::get(node_name+"/max_detect_range",nscans))
+  {
+	ROS_WARN("Maximum range for obstacle detection not defined.\nUsing default value!");
+	range=range;	
+  }
 /**
  * Error Handling!
  */
-  //Omit after fixing parameters input
-  min_obstacle_rad=0.6;
-  max_obstacle_rad=0.9;
-  //
-  if(min_obstacle_rad>=max_obstacle_rad)
+ if(min_obstacle_rad>=max_obstacle_rad)
   {
 	ROS_ERROR("Minimum Obstacle Radius(%g) cannot be greater than Maximum Obstacle Radius(%g).\nExiting....",min_obstacle_rad,max_obstacle_rad);
 	return -1;
@@ -90,31 +101,42 @@ int main(int argc, char **argv)
 	for(theta=-1.570796327;theta<=1.570796327;theta+=scan.angle_increment)
 	{
 		idx=cvRound((theta-scan.angle_min)/scan.angle_increment);
-		if(scan.ranges[idx]>25.0)
+		if(scan.ranges[idx]>range)
 		  continue;
 		r=scan.ranges[idx];
-		cv::Point center(cvRound(1120.0+(r*sin(theta)*1120.0/25.0)),cvRound(1120.0-(r*cos(theta)*1120.0/25.0)));
-		cv::circle(mask,center,cvRound(min_obstacle_rad*1120.0/25.0),cv::Scalar(1),cvRound((max_obstacle_rad-min_obstacle_rad)*1120.0/25.0),8,0);
+		center= cv::Point(cvRound(1120.0+(r*sin(theta)*1120.0/range)),cvRound(1120.0-(r*cos(theta)*1120.0/range)));
+		cv::circle(mask,center,cvRound(min_obstacle_rad*1120.0/range),cv::Scalar(1),cvRound((max_obstacle_rad-min_obstacle_rad)*1120.0/range),8,0);
 		acc=acc+mask;
 		mask=cv::Mat::zeros(1121,2241,CV_8UC1);
 	}
+	
+	if(accs.size()>=nscans)
+	  accs.pop_front();
+	for(i=0;i<accs.size();i++)
+	  acc+=accs[i];
+	accs.push_back(acc);
 /**
  * DEBUG TEST 1: Displaying the Accumulator Matrix as an Image and comparing it with Scan Data by running on a .bag file
- */
- 	cv::imshow("Obstacle Detection DEBUG 1",acc);
- 	cv::waitKey(1);
+ *
+	if(accs.size()>=nscans)
+	{
+		cv::imshow("Obstacle Detection DEBUG 1",acc);
+ 		cv::waitKey(0);
+	}
 /**
  * DEBUG TEST 1 RESULT: Expected Output obtained after certain modifications
  *              STATUS: Unable to obtain parameters
- */	
+ */
+	circles.clear();
+	ROS_ERROR("No. of Obstacles detected=%d",circles.size());	
 	for(i=0;i<1121;i++)
 	  for(j=0;j<2241;j++)
 	  {
 		if(acc.at<uchar>(i,j)>=threshold)
 		{
 			sum_i=sum_xi=sum_yi=0;
-			for(k=-2;k<=2;k++)
-			  for(l=-2;l<=2;l++)
+			for(k=-5;k<=4;k++)
+			  for(l=-5;l<=4;l++)
 			  {
 				intensity=acc.at<uchar>(i+k,j+l);
 				acc.at<uchar>(i+k,j+l)=0;
@@ -124,21 +146,15 @@ int main(int argc, char **argv)
 			  }
 			x_c=double(sum_xi)/double(sum_i);
 			y_c=double(sum_yi)/double(sum_i);
-			circles.push_back(cv::Vec3f((x_c-1120.0)*25.0/1120.0,(1120.0-y_c)*25.0/1120.0,0));
+			circles.push_back(cv::Vec3f((x_c-1120.0)*range/1120.0,(1120.0-y_c)*range/1120.0,0));
 		}
 	  }
-/**
- * DEBUG TEST 2: Analysis of the contents of the Vector circles and comparing it with the Accumulator Matrix
- */	
-
-/**
- * DEBUG TEST 2 STATUS: Compiled Successfully
- */
-	count=(int*)calloc(circles.size(),sizeof(int));
+	ROS_ERROR("No. of Obstacles detected=%d",circles.size());
+	count=(int*)calloc(circles.size(),sizeof(int));	
 	for(theta=-1.570796327;theta<=1.570796327;theta+=scan.angle_increment)
 	{
 		idx=cvRound((theta-scan.angle_min)/scan.angle_increment);
-		if(scan.ranges[idx]>25.0)
+		if(scan.ranges[idx]>range)
 		  continue;
 		r=scan.ranges[idx];
 		for(i=0;i<circles.size();i++)
@@ -154,9 +170,20 @@ int main(int argc, char **argv)
 	for(i=0;i<circles.size();i++)
 	  circles[i][2]/=count[i];
 /**
- * DEBUG TEST 3: Analysis of No. of points giving a particular center and comparing the obtained radius with Accumulator data
+ * DEBUG TEST 3: Analysis of No. of scan points giving a particular center and comparing the obtained radius with Accumulator data
  */
-
+	cv::Mat debug=cv::Mat::zeros(1121,2241,CV_8UC3);
+	cv::Point deb_cent;
+	for(i=0;i<circles.size();i++)
+	{
+		deb_cent=cv::Point(cvRound(1120.0+(circles[i][0]*1120.0/range)),cvRound(1120.0-(circles[i][1]*1120.0/range)));
+		cv::circle(mask,deb_cent,cvRound(circles[i][2]*1120.0/range),cv::Scalar(255,255,0),20,8,0);
+	}
+	if(accs.size()>=nscans)
+	{
+		cv::imshow("Obstacle Detection DEBUG 1",debug);
+ 		cv::waitKey(0);
+	}
 /**
  * DEBUG TEST 3 STATUS: Compiled Successfully
  */
